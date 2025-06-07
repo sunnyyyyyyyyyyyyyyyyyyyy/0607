d2ba47ea2978c694ad6e43ad8218902ff616e8bb
sunnyyyyyyyyyyyyyyyyyyyy <sunnycheng6222@gmail.com>
2025-06-07 15:39:32 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,16 +16,24 @@ let questions = [
 ];
 
 let currentQuestion = 0;
-let gameState = "start";
+let gameState = "waiting"; // 新增狀態：等待開始
 
 function setup() {
   createCanvas(640, 480);
+  
+  // 開始時不立即啟動攝影機和faceapi
+  // 攝影機跟faceapi會在 startGame() 被呼叫時建立
+}
+
+function startGame() {
   video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
 
   const faceOptions = { withLandmarks: true, withExpressions: false, withDescriptors: false };
   faceapi = ml5.faceApi(video, faceOptions, modelReady);
+
+  gameState = "start";
 }
 
 function modelReady() {
@@ -45,30 +53,69 @@ function gotResults(err, result) {
 
 function draw() {
   background(220);
-  image(video, 0, 0, width, height);
 
-  if (gameState === "start") {
-    drawQuestion();
-    detectFaceSelection();
-  } else if (gameState === "correct") {
-    fill(0, 255, 0);
-    textSize(32);
-    textAlign(CENTER, CENTER);
-    text("正確！", width / 2, height / 2);
-    setTimeout(nextQuestion, 1500);
-  } else if (gameState === "wrong") {
-    fill(255, 0, 0);
-    textSize(32);
-    textAlign(CENTER, CENTER);
-    text("答錯囉！", width / 2, height / 2);
-    setTimeout(nextQuestion, 1500);
+  if (gameState === "waiting") {
+    drawStartScreen();
+  } else {
+    // 鏡像翻轉畫面：先平移寬度，再scale X軸 -1
+    push();
+    translate(width, 0);
+    scale(-1, 1);
+
+    image(video, 0, 0, width, height);
+
+    if (detections.length > 0) {
+      drawFaceBox();
+    }
+    pop();
+
+    if (gameState === "start") {
+      drawQuestion();
+      detectFaceSelection();
+    } else if (gameState === "correct") {
+      fill(0, 255, 0);
+      textSize(32);
+      textAlign(CENTER, CENTER);
+      text("正確！", width / 2, height / 2);
+      setTimeout(nextQuestion, 1500);
+    } else if (gameState === "wrong") {
+      fill(255, 0, 0);
+      textSize(32);
+      textAlign(CENTER, CENTER);
+      text("答錯囉！", width / 2, height / 2);
+      setTimeout(nextQuestion, 1500);
+    }
+  }
+}
+
+function drawStartScreen() {
+  background(50, 100, 200);
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text("點擊開始遊戲", width / 2, height / 2 - 50);
+  
+  // 畫按鈕
+  fill(0, 200, 100);
+  rect(width/2 - 80, height/2, 160, 60, 20);
+  fill(255);
+  textSize(24);
+  text("開始", width / 2, height / 2 + 30);
+}
+
+function mousePressed() {
+  if (gameState === "waiting") {
+    // 判斷滑鼠是否點擊到按鈕範圍
+    if (mouseX > width/2 - 80 && mouseX < width/2 + 80 && mouseY > height/2 && mouseY < height/2 + 60) {
+      startGame();
+    }
   }
 }
 
 function drawQuestion() {
   let q = questions[currentQuestion];
 
-  fill(255);
+  fill(255, 200);
   rect(0, 0, width, 60);
   fill(0);
   textSize(20);
@@ -84,19 +131,26 @@ function drawQuestion() {
   }
 }
 
+function drawFaceBox() {
+  let alignedRect = detections[0].alignedRect;
+  let { x, y, width: w, height: h } = alignedRect._box;
+
+  // 因為鏡像畫面，X座標也要反轉
+  let centerX = width - (x + w / 2);
+  let centerY = y + h / 2;
+
+  fill(0, 255, 0);
+  ellipse(centerX, centerY, 20, 20);
+}
+
 function detectFaceSelection() {
   if (detections.length > 0) {
     let alignedRect = detections[0].alignedRect;
     let { x, y, width: w, height: h } = alignedRect._box;
 
-    // 用臉中心點作為指標
-    let centerX = x + w / 2;
+    let centerX = width - (x + w / 2); // 反轉X座標
     let centerY = y + h / 2;
 
-    fill(0, 255, 0);
-    ellipse(centerX, centerY, 20, 20); // 顯示臉部中點
-
-    // 判斷是否碰到選項
     let q = questions[currentQuestion];
     for (let i = 0; i < q.options.length; i++) {
       let optionY = 100 + i * 100;
